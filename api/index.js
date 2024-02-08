c5877d98dded4d10aa37a6e8449429757549c87f
Tembelihle Biyana <127286965+Tembelihl@users.noreply.github.com>
2024-02-08 23:16:20 +0200

    added folders and installed packages

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+const jwt = require("jsonwebtoken");
+
+mongoose.connect(
+    "mongodb+srv://lihlebiyana:biyana123@cluster0.dooaoqq.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+
+  .then(() => {
+    console.log("Connect to Mongo Db");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
